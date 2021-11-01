9c0ae34ee883b8e1d2eb5fb32a9f162b884a9ad9
colin <colingdegroat@gmail.com>
2021-11-01 17:58:31 -0400

    blah

--- WhyIsMyVSSoHatefulAboutAddingAProject/Program.cs
+++ WhyIsMyVSSoHatefulAboutAddingAProject/Program.cs
@@ -7,6 +7,7 @@ namespace WhyIsMyVSSoHatefulAboutAddingAProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //change something
         }
     }
 }
